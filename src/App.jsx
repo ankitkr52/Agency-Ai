2af2ffcf3443b1f9824586af990135d377859f16
ankitkr52 <ankit72p@gmail.com>
2026-01-28 16:58:27 +0530

    fixed cursor transisation

--- src/App.jsx
+++ src/App.jsx
@@ -48,10 +48,6 @@ const App = () => {
   };
 }, []);
 
-
-
-
-
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme') || 'light';
     setTheme(savedTheme);
@@ -79,7 +75,7 @@ const App = () => {
       <ContactUs />
       <Footer theme={theme} />
       {/* custom cursor ring */}
-      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'> </div>
+      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]' style={{transition:'transform 0.1s ease-out'}}> </div>
 
       {/* custom cursor dot */}
       <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full border border-primary pointer-events-none z-[9999]'>
