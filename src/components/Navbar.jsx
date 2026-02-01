b3dd4bf0791aafcf1252f19fc4e73e396ba9310a
ankitkr52 <ankit72p@gmail.com>
2026-02-01 11:02:29 +0530

    fixed animation in navbar and hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import assets from '../assets/assets'
 import ThemeToggleBtn1 from './ThemeToggleBtn1'
-
+import { motion } from "motion/react"
 
 const Navbar = ({ theme, setTheme }) => {
     const [sidebarOpen, setSidebarOpen] = useState(false)
     return (
-        <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+        <motion.div
+        initial={{opacity:0,y:-50}}
+        animate={{opacity:1,y:0}}
+        transition={{duration:0.6,ease:'easeout'}}
+        className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
 
 
             <img src={theme === 'light' ? assets.logo : assets.logo_dark} className='w-32 sm:w-40' alt='Agency.ai' />
@@ -36,7 +40,7 @@ const Navbar = ({ theme, setTheme }) => {
                 </a>
             </div>
 
-        </div>
+        </motion.div>
     )
 }
 
